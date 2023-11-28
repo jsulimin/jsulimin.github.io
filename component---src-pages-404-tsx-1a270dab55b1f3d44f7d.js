2449ec1b8f60204b4b1ca6b959b55beae93f2554
Josephine Sulimin <josephinesulimin@Josephines-MacBook-Pro.local>
2023-11-28 07:58:11 -0500

    Updates

---
+++ component---src-pages-404-tsx-1a270dab55b1f3d44f7d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjodie=self.webpackChunkjodie||[]).push([[218],{2513:function(e,t,l){l.r(t);var n=l(7294),u=l(8823);t.default=e=>n.createElement(u.Z,null,n.createElement("div",{style:{textAlign:"center"}},n.createElement("h1",null,"404"),n.createElement("p",null,"Page not found.")))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1a270dab55b1f3d44f7d.js.map
